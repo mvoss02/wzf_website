3cf0843c09ccd6e63e960c479474871df2eb69d7
mvoss02 <m_l_voss@web.de>
2024-06-14 09:41:51 +0200

    Changed the backend access

--- js/script.js
+++ js/script.js
@@ -100,7 +100,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function fetchGames(date, league) {
-        const apiUrl = 'https://8a49pchhxd.execute-api.eu-central-1.amazonaws.com/dev'; //process.env.BACKEND_URL;
+        const apiUrl = 'https://wlq8y13khl.execute-api.eu-central-1.amazonaws.com/production'; //process.env.BACKEND_URL;
 
         let requestData = {
             date: date
